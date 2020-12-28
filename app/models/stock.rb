22ffabd7f1729058348928045d1553819e035e4b
ext-nomad <i.dwanderer@gmail.com>
2020-12-28 17:19:42 +0300

    style/ search results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,8 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.dig(:iex, :secret),
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol,
+        name: client.company(ticker_symbol).company_name,
+        last_price: client.price(ticker_symbol))
   end
 end
