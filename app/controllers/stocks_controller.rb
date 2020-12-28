030c2436c0ccbf2a4e2cc1291ca57ffa4aeb3e76
ext-nomad <i.dwanderer@gmail.com>
2020-12-28 17:32:33 +0300

    update/ processing exceptions on search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,14 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        redirect_to my_portfolio_path, alert: 'Please enter a symbol to search'
+      end
+    else
+      redirect_to my_portfolio_path, alert: 'Please enter a symbol to search'
+    end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,8 +5,12 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.dig(:iex, :secret),
       endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    new(ticker: ticker_symbol,
-        name: client.company(ticker_symbol).company_name,
-        last_price: client.price(ticker_symbol))
+    begin
+      new(ticker: ticker_symbol,
+          name: client.company(ticker_symbol).company_name,
+          last_price: client.price(ticker_symbol))
+    rescue StandardError => e
+      nil
+    end
   end
 end
