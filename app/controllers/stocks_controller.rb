5f7a7eac843a1c9c472c701ae9389c7dc353c2fa
ext-nomad <i.dwanderer@gmail.com>
2020-12-29 16:15:10 +0300

    update/ setup js responce

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,7 +3,9 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
         redirect_to my_portfolio_path, alert: 'Please enter a symbol to search'
       end
