83b789827e1d149c5397d1c9cfec866c55c60a71
ext-nomad <i.dwanderer@gmail.com>
2020-12-29 18:48:52 +0300

    update/ refactoring js responding

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,34 @@
 class StocksController < ApplicationController
+  # def search
+  #   if params[:stock].present?
+  #     @stock = Stock.new_lookup(params[:stock])
+  #     if @stock
+  #       respond_to do |format|
+  #         format.js { render partial: 'users/result' }
+  #       end
+  #     else
+  #       respond_to do |format|
+  #         flash.now[:alert] = 'Please enter a valid symbol to search'
+  #         format.js { render partial: 'users/result' }
+  #       end
+  #     end
+  #   else
+  #     respond_to do |format|
+  #       flash.now[:alert] = 'Please enter a symbol to search'
+  #       format.js { render partial: 'users/result' }
+  #     end
+  #   end
+  # end
+
   def search
-    if params[:stock].present?
-      @stock = Stock.new_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
+    respond_to do |format|
+      if params[:stock].present?
+        @stock = Stock.new_lookup(params[:stock])
+        flash.now[:alert] = 'Please enter a valid symbol to search' unless @stock
       else
-        redirect_to my_portfolio_path, alert: 'Please enter a symbol to search'
+        flash.now[:alert] = 'Please enter a symbol to search'
       end
-    else
-      redirect_to my_portfolio_path, alert: 'Please enter a symbol to search'
+      format.js { render partial: 'users/result' }
     end
   end
 end
