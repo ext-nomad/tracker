2d7145d202b55d9159fca42b89d17f4827993d03
ext-nomad <i.dwanderer@gmail.com>
2021-01-03 20:04:55 +0300

    style/ cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      redirect_to my_portfolio_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 
-  def about
-  end
+  def about; end
 end
