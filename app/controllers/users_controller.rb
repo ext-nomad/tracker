2fa4abc6eb003ea8760563fb19695b031a67f5ba
ext-nomad <i.dwanderer@gmail.com>
2021-01-06 18:31:51 +0300

    update/ friends searching ajax response setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,16 @@ class UsersController < ApplicationController
   def my_friends
     @friends = current_user.friends
   end
+
+  def search
+    respond_to do |format|
+      if params[:friend].present?
+        @friend = User.find(2)
+        flash.now[:alert] = 'Please enter a valid name to search' unless @friend
+      else
+        flash.now[:alert] = 'Please enter a name to search'
+      end
+      format.js { render partial: 'users/friend_result' }
+    end
+  end
 end
