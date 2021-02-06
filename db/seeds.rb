6acfdd71960dc0420d417adc40026c2af15eb820
ext-nomad <i.dwanderer@gmail.com>
2021-02-06 12:22:58 +0300

    set up capistrano

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+  email = "example-#{n + 1}@example.com"
+  User.create!(email: email, password: email, first_name: first_name, last_name: last_name)
+end
+
+def rand_id(from, to)
+  Faker::Number.between(from: from, to: to)
+end
+
+3.times do
+  Friendship.create!([{ user_id: rand_id(1, 10), friend_id: rand_id(1, 10) }])
+end
